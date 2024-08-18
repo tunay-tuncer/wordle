efc874e4a4c7a87954d4ef4eea40ee214f00e323
tunay-tuncer <tunayekin.tuncer@gmail.com>
2024-08-19 02:03:31 +0300

    turkish version

--- script.js
+++ script.js
@@ -8,132 +8,187 @@ const thirdWord = document.querySelectorAll(".thirdWord div");
 const fourthWord = document.querySelectorAll(".fourthWord div");
 const fifthWord = document.querySelectorAll(".fifthWord div");
 
-const alertDiv = document.querySelector(".alertDiv")
-const resetGameButton = document.querySelector(".resetGameButton")
+const alertDiv = document.querySelector(".alertDiv");
+const resetGameButton = document.querySelector(".resetGameButton");
 
-const wordRowList = [firstWord ,secondWord ,thirdWord ,fourthWord , fifthWord];
+const wordRowList = [firstWord, secondWord, thirdWord, fourthWord, fifthWord];
 
 let currentLetter;
 let currentLetterIndex = 0;
 let currentWordRow = 0;
 
-const wordList = ["Apple","Beach","Brain", "Bread","Brush",,"Chest","Chair","Chord","Click","Clock","Cloud","Dance","Diary","Drink","Earth","Flute","Fruit","Ghost","Grape","Green","Happy","Heart","House","Juice","Light","Money","Music","Party","Pizza","Plant","Radio","River","Salad","Sheep","Shoes","Smile","Snack","Snake","Spice","Spoon","Storm","Table","Toast","Tiger","Train","Water","Whale","Wheel","Woman","World","Write","Youth"]
+let alertText = "";
+
+const wordList = [
+    "Afaki",
+    "Açlık",
+    "Abiye",
+    "Abbas",
+    "Balon",
+    "Bahri",
+    "Bahçe",
+    "Cacık",
+    "Camcı",
+    "Cıbıl",
+    "Cümle",
+    "Çöpçü",
+    "Çürük",
+    "Çinli",
+    "Çinko",
+    "Çözüm",
+    "Dilim",
+    "Daimi",
+    "Dilek",
+    "Dışkı",
+    "Ezber",
+    "Evlat",
+    "Enfes",
+    "Fosil",
+    "Felek",
+    "Gayet",
+    "Giyim",
+    "Gazoz",
+    "Hamak",
+    "Hoşaf",
+    "Hamsi",
+    "İnmek",
+    "İnkar",
+    "İbraz",
+    "Irkçı",
+    "Ilgaz",
+    "Jokey",
+    "Jarse",
+    "Kredi",
+    "Kalın",
+    "Kablo",
+    "Lüzum",
+    "Lotus",
+    "Leğen",
+    "Mevla",
+    "Masal",
+    "Melez",
+    "Nişan",
+    "Nalan",
+    "Ninni",
+    "Oğlak",
+    "Övmek",
+    "Ördek",
+    "Pilot",
+    "Posta",
+    "Rampa",
+    "Roman",
+    "Sakız",
+    "Savcı",
+    "Şifre",
+    "Tekne",
+    "Uzman",
+    "Üzgün",
+    "Vakıf",
+    "Yalın",
+    "Zehir",
+];
 
 let wordToCheck = "";
 let word = "";
 
-
-// let options = {
-//     method: 'GET',
-//     headers: { 'x-api-key': 'Mf2ty5UHkIb+6hN8kzYcQw==pGQqf1hZdY28ZtXG' }
-//   }
-  
-//   let url = 'https://api.api-ninjas.com/v1/randomword'
-  
-  
-//   fetch(url,options)
-//         .then(res => res.json()) //
-//         .then(data => {
-//           word = data.word
-//           console.log(word)
-//         })
-//         .catch(err => {
-//             console.log(`error ${err}`)
-//         }); 
-       
-
-function chooseWord(){
-    let randomWordIndex = Math.floor(Math.random()*wordList.length);
-    word = wordList[randomWordIndex].toUpperCase(); 
-    console.log(word)
+function chooseWord() {
+    let randomWordIndex = Math.floor(Math.random() * wordList.length);
+    word = wordList[randomWordIndex].toUpperCase();
+    console.log(word);
 }
 
-key.forEach((key)=>{
-    key.addEventListener("click", ()=>{
+key.forEach((key) => {
+    key.addEventListener("click", () => {
         currentLetter = key.textContent;
-        wordRowList[currentWordRow][currentLetterIndex].textContent = currentLetter;
-        currentLetterIndex < 5 ? currentLetterIndex ++ : currentLetterIndex = 4;
-    })
-})
- 
-function deleteLetter(){
-    currentLetterIndex > 0 ? currentLetterIndex-- : currentLetterIndex = 0;
+        wordRowList[currentWordRow][currentLetterIndex].textContent =
+            currentLetter;
+        currentLetterIndex < 5
+            ? currentLetterIndex++
+            : (currentLetterIndex = 4);
+    });
+});
+
+function deleteLetter() {
+    currentLetterIndex > 0 ? currentLetterIndex-- : (currentLetterIndex = 0);
     wordRowList[currentWordRow][currentLetterIndex].textContent = "";
 }
 
-function checkWordLength(){
-    for(let i=0; i<wordRowList[currentWordRow].length; i++){
+function checkWordLength() {
+    for (let i = 0; i < wordRowList[currentWordRow].length; i++) {
         wordToCheck += wordRowList[currentWordRow][i].textContent;
     }
-    if(wordToCheck.length < wordRowList[currentWordRow].length){
+    if (wordToCheck.length < wordRowList[currentWordRow].length) {
         console.log("5 harf yazacan");
         wordToCheck = "";
-    }
-
-    else{
-
-        if(word == wordToCheck){
+    } else {
+        if (word == wordToCheck) {
             checkLetterProperties();
             winGame();
-            console.log("kazandın");
+        } else {
+            checkLetterProperties();
         }
-        else{
-            checkLetterProperties()
-        }
-        currentWordRow < wordRowList.length ? currentWordRow++ :  gameOver();
+        currentWordRow < wordRowList.length ? currentWordRow++ : gameOver();
 
-        currentLetterIndex = 0
-    };
-    wordToCheck = ""
+        currentLetterIndex = 0;
+    }
+    wordToCheck = "";
 }
 
-function wrongLetterProperty(){
-    for(let i=0; i<wordToCheck.length; i++){
-        const charToCheck = wordToCheck[i]
-        if(word.indexOf(charToCheck) === -1){
-            document.querySelector(`.key${wordToCheck[i]}`).classList.add("wrongLetter");
-            // document.querySelector(`key${wordToCheck[i]}`).removeEventListener()
+function wrongLetterProperty() {
+    for (let i = 0; i < wordToCheck.length; i++) {
+        const charToCheck = wordToCheck[i];
+        if (word.indexOf(charToCheck) === -1) {
+            document
+                .querySelector(`.key${wordToCheck[i]}`)
+                .classList.add("wrongLetter");
             wordRowList[currentWordRow][i].classList.add("wrongBox");
         }
     }
 }
 
-function rightLetterProperty(){
-    for(let i=0; i<wordToCheck.length; i++){
-        if(word[i] == wordToCheck[i]){
-            document.querySelector(`.key${wordToCheck[i]}`).classList.add("rightLetter");
+function rightLetterProperty() {
+    for (let i = 0; i < wordToCheck.length; i++) {
+        if (word[i] == wordToCheck[i]) {
+            document
+                .querySelector(`.key${wordToCheck[i]}`)
+                .classList.add("rightLetter");
             wordRowList[currentWordRow][i].classList.add("rightBox");
         }
     }
 }
 
-function wrongPlaceProperty(){
-    for(let i=0; i<wordToCheck.length; i++){
-        if(word.includes(wordToCheck[i]) && word[i] != wordToCheck[i]){
-            document.querySelector(`.key${wordToCheck[i]}`).classList.add("wrongPlace");
+function wrongPlaceProperty() {
+    for (let i = 0; i < wordToCheck.length; i++) {
+        if (word.includes(wordToCheck[i]) && word[i] != wordToCheck[i]) {
+            document
+                .querySelector(`.key${wordToCheck[i]}`)
+                .classList.add("wrongPlace");
             wordRowList[currentWordRow][i].classList.add("wrongPlaceBox");
         }
     }
 }
 
-function checkLetterProperties(){
+function checkLetterProperties() {
     wrongLetterProperty();
     wrongPlaceProperty();
-    rightLetterProperty()
+    rightLetterProperty();
 }
 
-function gameOver(){
-    console.log("game over");
-}
-
-function winGame(){
+function gameOver() {
+    alertDiv.textContent = "Tekrar Dene!";
     alertDiv.style.animation = "winAnimation 5s";
     resetGameButton.style.display = "block";
 }
 
-function resetGame(){
-    for(let i=0; i<wordRowList.length; i++){
-        for(let j=0; j<firstWord.length; j++){
+function winGame() {
+    alertDiv.textContent = "EŞEK HANİFE!";
+    alertDiv.style.animation = "winAnimation 5s";
+    resetGameButton.style.display = "block";
+}
+
+function resetGame() {
+    for (let i = 0; i < wordRowList.length; i++) {
+        for (let j = 0; j < firstWord.length; j++) {
             wordRowList[i][j].textContent = "";
             wordRowList[i][j].classList.remove("rightBox");
             wordRowList[i][j].classList.remove("wrongPlaceBox");
@@ -141,7 +196,7 @@ function resetGame(){
         }
     }
 
-    for(let k=0; k<key.length; k++){
+    for (let k = 0; k < key.length; k++) {
         key[k].classList.remove("rightLetter");
         key[k].classList.remove("wrongLetter");
         key[k].classList.remove("wrongPlace");
@@ -156,6 +211,6 @@ function resetGame(){
 
 chooseWord();
 
-resetGameButton.addEventListener("click", resetGame)
-deleteKey.addEventListener("click", deleteLetter)
-enterKey.addEventListener("click",checkWordLength)
+resetGameButton.addEventListener("click", resetGame);
+deleteKey.addEventListener("click", deleteLetter);
+enterKey.addEventListener("click", checkWordLength);
